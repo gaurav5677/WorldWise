54c57f68813da20dab9c4e61e3352d97811b9a7e
Gaurav <gauravpatilfamily7@gmail.com>
2023-08-10 01:27:12 +0530

    First Routes Defined

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,8 +1,16 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+      </Routes>
     </BrowserRouter>
   );
 }
