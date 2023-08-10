741b3b4b87399407943a8677eb53f656f2828fd0
Gaurav <gauravpatilfamily7@gmail.com>
2023-08-10 23:35:42 +0530

    Finished Nav And Pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
-        <Route path="product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
